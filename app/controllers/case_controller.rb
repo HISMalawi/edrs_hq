baebc3b8c5aac4c3bc37909b7b8c16e3d10da78d
mwatha <mwathabwanali@gmail.com>
2017-01-15 16:33:19 +0200

    cleaning up code

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -1,8 +1,23 @@
 class CaseController < ApplicationController
   def open
-
-    @cases = Person.all.page(1).per(10).each
+    #@cases = Person.all.page(1).per(10).each
     @page = 1
+  end
+
+  def more_open_cases
+
+    cases = []
+    
+    (Person.all.page(params[:page_number]).per(10) || []).each do |person|
+      cases << {
+        first_name: person.first_name,
+        last_name:  person.last_name,
+        dob:        person.birthdate,
+        gender:     person.gender
+      }
+    end
+
+    render text: cases.to_json and return
   end
 
 end
