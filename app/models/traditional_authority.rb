928844398ca2bafbac2958a75fd4b5faaa5e1989
phundi <kennethkapundi1@gmail.com>
2017-01-26 11:17:30 +0200

    added new file

---
+++ app/models/traditional_authority.rb
@@ -0,0 +1,24 @@
+require 'couchrest_model'
+
+class TraditionalAuthority < CouchRest::Model::Base
+  property :district_id, String
+  property :name, String
+
+  timestamps!
+
+  design do
+    view :by__id
+    view :by_district_id,
+         :map => "function(doc){
+
+      	   			if(doc.type=='TraditionalAuthority'){
+
+      	   				emit(doc.district_id, {name : doc.name})
+      	   			}
+      	  	      }"
+    view :by_name
+    view :by_district_id_and_name
+
+  end
+
+end
