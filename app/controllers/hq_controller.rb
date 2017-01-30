07f01a40a16581100cad16c93a5c8ad120dd18cb
Blessings <blessings.mhango@gmail.com>
2017-01-30 13:44:49 +0200

    do print these

--- app/controllers/hq_controller.rb
+++ app/controllers/hq_controller.rb
@@ -67,6 +67,50 @@ class HqController < ApplicationController
   end
   
   def do_print_these
+     
+    selected = params[:selected].split("|")
+
+    paper_size = GlobalProperty.find("paper_size").value rescue "A4"
+    
+    if paper_size == "A4"
+       zoom = 0.83
+    elsif paper_size == "A5"
+       zoom = 0.6
+    end
+     
+    selected.each do |key|
+
+      person = Person.find(key.strip)
+
+      next if person.blank?
+      
+      person.update_attributes(:record_status => "PRINTED",
+                               :request_status => "CLOSED", 
+                               :printed_at => Time.now,
+                               :date_certificate_issued => Date.today.strftime("%Y-%m-%d") ) if person.record_status != 'PRINTED'
+      
+     
+      id = person.id
+      
+      print_url = "wkhtmltopdf --zoom #{zoom} --page-size #{paper_size} --username #{CONFIG["print_user"]} --password #{CONFIG["print_password"]} #{CONFIG["protocol"]}://#{request.env["SERVER_NAME"]}:#{request.env["SERVER_PORT"]}/death_certificate/#{id} #{CONFIG['certificates_path']}#{id}.pdf\n"    
+      
+      t4 = Thread.new {
+
+        Kernel.system print_url
+
+        sleep(4)
+
+        Kernel.system "lp -d #{params[:printer_name]} #{CONFIG['certificates_path']}#{id}.pdf\n"
+
+        sleep(5)
+        
+      }
+
+      sleep(1)
+
+    end
+    
+   # redirect_to "/hq/print_certificates_datatable" and return
   
   end
   
