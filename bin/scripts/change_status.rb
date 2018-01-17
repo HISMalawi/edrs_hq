9123901dab42b4b1b74341e2d12f09900132ea4f
cranberry <manzymkamba@gmail.com>
2018-01-17 11:40:28 +0200

    Change status script

--- bin/scripts/change_status.rb
+++ bin/scripts/change_status.rb
@@ -1,10 +1,13 @@
-PersonRecordStatus.all.each do |status|
-	status.voided = 1 
-	if status.save
-		        PersonRecordStatus.create({
-                                  :person_record_id => status.person_record_id.to_s,
-                                  :status => "HQ COMPLETE",
-                                  :district_code => status.district_code,
-                                  :creator => status.creator})
+Person.all.each do |person| 
+	record_status = nil
+	PersonRecordStatus.by_person_record_id.key(person.id).each do |status|
+		record_status = status
+		status.voided = 1 
+		status.save
 	end
+	PersonRecordStatus.create({
+	                                  :person_record_id => record_status.person_record_id.to_s,
+	                                  :status => "HQ COMPLETE",
+	                                  :district_code => record_status.district_code,
+	                                  :creator =>record_status.creator})
 end
