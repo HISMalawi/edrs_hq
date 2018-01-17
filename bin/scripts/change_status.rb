cbb14ee09d228750103053a6e25fa81181988769
cranberry <manzymkamba@gmail.com>
2018-01-17 11:28:59 +0200

    Change status

--- bin/scripts/change_status.rb
+++ bin/scripts/change_status.rb
@@ -2,7 +2,7 @@ PersonRecordStatus.all.each do |status|
 	status.voided = 1 
 	if status.save
 		        PersonRecordStatus.create({
-                                  :person_record_id => status.person_status_id.to_s,
+                                  :person_record_id => status.person_record_id.to_s,
                                   :status => "HQ COMPLETE",
                                   :district_code => status.district_code,
                                   :creator => status.creator})
