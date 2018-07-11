54a9020a253f498f9f0207008d6ba987c25c717b
cranberry <manzymkamba@gmail.com>
2018-07-11 12:09:15 +0200

    Date of approval

--- app/controllers/hq_controller.rb
+++ app/controllers/hq_controller.rb
@@ -304,10 +304,12 @@ class HqController < ApplicationController
     @drn = @person.drn
     @den = @person.den
 
-    if PersonRecordStatus.by_person_record_id_and_status.key([@person.id,"HQ ACTIVE"]).first.present?
-         @date_registered = PersonRecordStatus.by_person_record_id_and_status.key([@person.id,"HQ ACTIVE"]).first.created_at
-    else
-         @date_registered = @person.created_at
+    @date_registered = @person.created_at
+    PersonRecordStatus.by_person_record_id.key(@person.id).each.sort_by{|s| s.created_at}.each do |state|
+      if state.status == "HQ ACTIVE"
+          @date_registered = state.created_at
+          break;
+      end
     end
    
     @barcode = File.read("#{CONFIG['barcodes_path']}#{@person.id}.png") rescue nil
@@ -323,11 +325,12 @@ class HqController < ApplicationController
     @drn = @person.drn
     @den = @person.den
     @barcode = File.read("#{CONFIG['barcodes_path']}#{@person.id}.png") rescue nil
-    
+
     @date_registered = @person.created_at
     PersonRecordStatus.by_person_record_id.key(@person.id).each.sort_by{|s| s.created_at}.each do |state|
       if state.status == "HQ ACTIVE"
           @date_registered = state.created_at
+          break;
       end
     end
     
