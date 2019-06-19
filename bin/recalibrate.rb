2e38f094a2c443587750465f517a546aa63d672e
Emmanuel Mkambankhani <manzymkamba@gmail.com>
2019-06-19 12:59:59 +0200

    Recalibrate changed

--- bin/recalibrate.rb
+++ bin/recalibrate.rb
@@ -1,4 +1,5 @@
 #Load metadata
+=begin
 puts "########### LOADING METADATA PLEASE WAIT ITS GOING TO TAKE A MOMENT ###########"
 `cd #{Rails.root} && bundle exec rails r bin/scripts/load_metadata_to_sql.rb`
 puts "########### DONE LOADING METADATA ###########"
@@ -12,3 +13,7 @@ puts "##########################################################################
 puts "################################# ADDING ENTRIES TO CRONTAB #################################"
 puts `cd #{Rails.root} && bundle exec rails r bin/scripts/add_to_crontab.rb`
 puts "################################# DONE RECALIBRATION #################################"
+=end
+alter_table_couchdb_sequence_and_audit_trail = "ALTER TABLE couchdb_sequence MODIFY COLUMN seq TEXT;
+												ALTER TABLE audit_trail MODIFY COLUMN change_log TEXT;"
+SimpleSQL.query_exec(alter_table_couchdb_sequence_and_audit_trail); 
