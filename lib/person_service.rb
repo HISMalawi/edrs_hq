0a8bef443c0f22691c3343b9e9b8594339eaa6e4
Emmanuel Mkambankhani <emmanuel.mkambankhani@gmail.com>
2020-09-08 08:11:23 +0200

    QR Code data optimised

--- lib/person_service.rb
+++ lib/person_service.rb
@@ -2,13 +2,13 @@ require "rails"
 class PersonService
 	def self.qr_code_data(id)
 	    person = Person.find(id)
-	    date_registered = person.created_at
-	    PersonRecordStatus.by_person_record_id.key(person.id).each.sort_by{|s| s.created_at}.each do |state|
-	      if state.status == "HQ ACTIVE"
-	          date_registered = state.created_at
-	          break;
-	      end
-	    end
+		date_registered = person.created_at
+
+		registered_state = RecordStatus.where(person_record_id: person.id, status: 'HQ ACTIVE').order(:created_at).first
+		if registered_state.present?
+			date_registered = registered_state.created_at
+		end
+
 	    str = "05~#{person.npid}-#{person.den}-#{person.drn}"
 	    str += "~#{person.person_name}~#{person.birthdate.to_date.strftime("%d-%b-%Y")}~#{person.gender.first}"
 	    str += "~#{person.nationality}"    
